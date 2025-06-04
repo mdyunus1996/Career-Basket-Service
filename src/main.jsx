5c768c04ed3b5648ef3b2396a4ebe1a8d2d70050
mdyunus1996 <yunus@navadhiti.com>
2025-06-04 13:18:39 +0530

    Refactor main.jsx to improve import organization and set Router basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "@/App";
+import "@/index.css";
+import { Toaster } from "@/components/ui/toaster";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from '@/App';
-import '@/index.css';
-import { Toaster } from '@/components/ui/toaster';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/Career-Basket-Service/">
       <App />
       <Toaster />
     </Router>
