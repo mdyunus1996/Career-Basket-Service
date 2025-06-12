6332a398960b475e261090211ae0eed7a0702db1
mdyunus1996 <yunus@navadhiti.com>
2025-06-12 17:39:33 +0530

    Refactor HomePage layout by removing unused sections and reordering components for improved structure

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import HeroSection from "@/components/sections/HeroSection";
-import CoreValuesSection from "@/components/sections/CoreValuesSection";
 import ServicesSection from "@/components/sections/ServicesSection";
 import FoundersVisionSection from "@/components/sections/FoundersVisionSection";
-import SubsidiariesSection from "@/components/sections/SubsidiariesSection";
 import SuccessStoriesSection from "@/components/sections/SuccessStoriesSection";
 import ClientPartnershipsSection from "@/components/sections/ClientPartnershipsSection";
 import ContactSection from "@/components/sections/ContactSection"; // Added a contact section
+import AboutUsSection from "../components/sections/AboutUsSection";
+import OurVerticalsSection from "../components/sections/OurVerticalsSection";
+import PhotoGallerySection from "../components/sections/PhotoGallerySection";
+import CBSFoundationSection from "../components/sections/CBSFoundationSection";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
-      <CoreValuesSection />
-      <ServicesSection />
-      <SubsidiariesSection />
+      <AboutUsSection />
       <SuccessStoriesSection />
+      <OurVerticalsSection />
+      <CBSFoundationSection />
       <ClientPartnershipsSection />
-      <FoundersVisionSection />
+      <ServicesSection />
+      <PhotoGallerySection />
       <ContactSection />
     </>
   );
